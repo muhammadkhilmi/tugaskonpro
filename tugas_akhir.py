e5a6e2d7eead57f0fe4b26966615e5f9b6f9b518
Khilmi <33856340+muhammadkhilmi@users.noreply.github.com>
2017-12-17 16:22:13 +0700

    File Baru

--- tugas_akhir.py
+++ tugas_akhir.py
@@ -4,3 +4,15 @@ def randnumber():
 	print("Tebak angka antara 1 sd 100")
 	number = random.randint(1,100)
 	return number
+
+def userinput():
+	putar = True
+	while putar:
+		guest = input("Masukkan angka tebakan: ")
+		try:
+	   		number = int(guest)
+		except ValueError:
+		    print("Input harus berupa angka")
+		else:
+		    return int(guest)
+		    putar = False
